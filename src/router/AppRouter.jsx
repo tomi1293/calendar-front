0eb7ef68f4470c9b299d82bdd813a3497cf94a9a
Tomas Martinez <martineztomi93@gmail.com>
2023-07-09 18:45:54 -0300

    Actualizamos el Readme

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth";
 import { CalendarPage } from "../calendar";
+import { getEnvVariables } from "../helpers";
 
 export const AppRouter = () => {
   
